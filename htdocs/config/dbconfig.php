df45df791677ac3235b3004140f66c0c5f3330e0
hiroaki-kitagawa <hirock17l@icloud.com>
2020-03-25 20:14:07 +0900

    modify dsn charset

--- htdocs/config/dbconfig.php
+++ htdocs/config/dbconfig.php
@@ -1,5 +1,5 @@
 <?php
-define('DSN', 'mysql:host=mysql;dbname=todolist');
+define('DSN', 'mysql:host=mysql;dbname=todolist;charset=utf8');
 define('DB_USER', 'root');
 define('DB_PASS', 'password');
 
