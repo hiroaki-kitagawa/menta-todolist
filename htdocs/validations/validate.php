9c34757d540865ac40663cea6bf2bd1a255f36f5
hiroaki-kitagawa <hirock17l@icloud.com>
2020-04-21 20:49:58 +0900

    htmlspecialchars

--- htdocs/validations/validate.php
+++ htdocs/validations/validate.php
@@ -7,12 +7,12 @@ class Validation {
         if ($params['title'] === '') {
             $_SESSION['errormsg']['title'] = "タイトルが入力されていません。";
         } else {
-            $_SESSION['success']['title'] = $params['title'];
+            $_SESSION['success']['title'] = htmlspecialchars($params['title'], ENT_QUOTES, 'utf-8');
         }
         if ($params['detail'] === '') {
             $_SESSION['errormsg']['detail'] = "内容が入力されていません。";
         } else {
-            $_SESSION['success']['detail'] = $params['detail'];
+            $_SESSION['success']['detail'] = htmlspecialchars($params['detail'], ENT_QUOTES, 'utf-8');
         }
     }
 
